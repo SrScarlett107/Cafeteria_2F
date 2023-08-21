ee07298e362a483daf0dd726c23aa300e6726b3d
Matheus Nascimento <edu25070@gmail.com>
2023-08-21 09:34:57 -0300

    teste

---
+++ src/test/java/com/Cafeteria/INF2FM/myproject2f/Myproject2fApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cafeteria.INF2FM.myproject2f;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myproject2fApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
