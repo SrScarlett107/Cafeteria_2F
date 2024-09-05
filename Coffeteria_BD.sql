e003ad9e9c178d047ee35c9f3607624c8001c1b1
Matheus Nascimento da Silva <rm86584@estudante.fieb.edu.br>
2024-09-04 22:27:21 -0300

    teste

---
+++ Coffeteria_BD.sql
@@ -0,0 +1,39 @@
+use master if exists(
+select * from SYS.databases where name = 'myCafeteria2f')
+drop database myCafeteria2f
+go
+create database myCafeteria2f
+go
+use myCafeteria2f
+
+
+
+create table categorias(
+id bigint identity primary key,
+categoria varchar(30),
+
+)
+create table cardapios (
+id bigint  identity primary key,
+nome varchar(20),
+foto varbinary(max),
+descricao varchar(200),
+valor decimal(3,2),
+dataDoCadastro date,
+codStatusCardapio bit,
+id_categoria bigint,
+Constraint fk_categoria foreign key (id_categoria) references categorias(id)
+
+)
+
+
+create table adms(
+id bigint identity primary key,
+usuario varchar(20),
+senha int,
+
+)
+
+insert into adms values('Mateus', 1234456 )
+insert into adms values('Gisely', 6543211)
+insert into adms values('Matheus', 250706)
--- marvel-corner-box-art/README.md
+++
@@ -1,4 +0,0 @@
-# Marvel Corner Box Art
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/aryancodeworm/pen/vYJzMgB](https://codepen.io/aryancodeworm/pen/vYJzMgB).
-
